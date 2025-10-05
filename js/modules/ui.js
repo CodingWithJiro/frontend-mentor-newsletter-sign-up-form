b54e6e93fb14a925c0c3a2521afa123040dbc57b
CodingWithJiro <chavezelmar03@gmail.com>
2025-10-05 20:19:52 +0800

    refactor(js): renamed variable names and functions for easier debugging in ui.js

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -59,7 +59,7 @@ const themeButtonHTML = `
         ></button>
 `;
 const listElements = document.querySelectorAll(".card__list");
-const checkIconList = `<svg class="card__icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155"/><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"/></g></svg>`;
+const checkIconSVG = `<svg class="card__icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155"/><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"/></g></svg>`;
 const successIconSVG = ` <svg
             class="thanks__icon"
             aria-hidden="true"
@@ -89,9 +89,9 @@ function addThemeHTML() {
   theme.innerHTML = themeIconLightHTML + themeIconDarkHTML + themeButtonHTML;
 }
 
-function addCheckIconsHTML() {
+function addCheckIconSVG() {
   listElements.forEach((list) => {
-    list.insertAdjacentHTML("afterbegin", checkIconList);
+    list.insertAdjacentHTML("afterbegin", checkIconSVG);
   });
 }
 
@@ -101,6 +101,6 @@ function addSuccessIconSVG() {
 
 export function initUI() {
   addThemeHTML();
-  addCheckIconsHTML();
+  addCheckIconSVG();
   addSuccessIconSVG();
 }
