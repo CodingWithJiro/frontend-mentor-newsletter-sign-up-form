43dce9d794c32d2ce5242badf7f52365c04cc275
CodingWithJiro <chavezelmar03@gmail.com>
2025-10-05 18:07:31 +0800

    feat(js): select .card__list elements and create check icon HTML text

--- js/modules/ui.js
+++ js/modules/ui.js
@@ -58,6 +58,8 @@ const themeButtonHTML = `
           class="theme__button"
         ></button>
 `;
+const listElements = document.querySelectorAll(".card__list");
+const checkIconList = `<svg class="card__icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21"><g fill="none"><circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155"/><path stroke="#FFF" stroke-width="2" d="M6 11.381 8.735 14 15 8"/></g></svg>`;
 
 function addThemeHTML() {
   theme.innerHTML = themeIconLightHTML + themeIconDarkHTML + themeButtonHTML;
