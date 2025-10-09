4964b1026d398d28edc4952e5ecfa736128e2b22
CodingWithJiro <chavezelmar03@gmail.com>
2025-10-09 23:25:47 +0800

    feat(js): create initForm() function in form.js and export it

--- js/modules/form.js
+++ js/modules/form.js
@@ -1,3 +1,7 @@
 // * FORM.JS SCRIPT
 const form = document.querySelector(".card__form");
 const input = document.querySelector(".card__input");
+
+export function initForm() {
+  // WRITE FUNCTIONS HERE
+}
