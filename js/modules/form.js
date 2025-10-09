0b70f9d3e4e8fd45b505fb5776984de24bb25eca
CodingWithJiro <chavezelmar03@gmail.com>
2025-10-09 23:35:31 +0800

    feat(js): add emailRegex in form.js for validation

--- js/modules/form.js
+++ js/modules/form.js
@@ -1,6 +1,7 @@
 // * FORM.JS SCRIPT
 const form = document.querySelector(".card__form");
 const input = document.querySelector(".card__input");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 export function initForm() {
   // WRITE FUNCTIONS HERE
