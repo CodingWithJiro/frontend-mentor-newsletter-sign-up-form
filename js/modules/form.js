cde33ebcf0c28b78f8a3a8512b7520540b81d3f9
CodingWithJiro <chavezelmar03@gmail.com>
2025-10-09 23:42:51 +0800

    feat(js): select .card and .thanks from DOM in form.js

--- js/modules/form.js
+++ js/modules/form.js
@@ -2,6 +2,8 @@
 const form = document.querySelector(".card__form");
 const input = document.querySelector(".card__input");
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const card = document.querySelector(".card");
+const thanks = document.querySelector(".thanks");
 
 export function initForm() {
   form.addEventListener("submit", (e) => {
